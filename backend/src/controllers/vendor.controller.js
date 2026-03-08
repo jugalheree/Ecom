eeca24ae4256547cd67009287bf758c1cb7765dd
Bhavesh Parmar <161071443+Bhavesh7755@users.noreply.github.com>
2026-03-08 12:42:11 +0530

    Clean up vendor.controller.js by removing comments
    Removed commented-out code for getProducts function.

--- backend/src/controllers/vendor.controller.js
+++ backend/src/controllers/vendor.controller.js
@@ -404,25 +404,6 @@ export const uploadProductImages = asyncHandler(async (req, res) => {
 });
 
 
-// export const getProducts  = asyncHandler(async (req,res) => {
-//   const userId = req.user._id;
-//   if(!userId){
-//     throw new ApiError(401, "Unauthorized");
-//   }
-//   const vendor = await Vendor.findOne({ userId });
-
-//   if(!vendor){
-//     throw new ApiError(401, "No Vender is found by this user Id");
-//   }
-
-//   const products = await  Product.find({ vendorId: vendor._id });
-
-//   if(!products || products.length === 0){
-//     return res.status(200).json(new ApiResponse(200, products, "No product found"));
-//   }
-//   return res.status(200).json(new ApiResponse(200, products, "Products fetched successfully"));
-// })
-
 
 
 
